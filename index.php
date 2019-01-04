c2c23b2719addf463d527d87241ef66a6b2e99a7
Celine Leano <cemleano@gmail.com>
2019-01-04 13:34:43 -0500

    Created a printArray method that takes an array as a parameter

--- index.php
+++ index.php
@@ -6,5 +6,13 @@
 </head>
 <body>
     <h1>Pair Program 1</h1>
+
+    <?php
+    $numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+    function printArray($array) {
+        echo $array;
+    }
+    ?>
 </body>
 </html>
