ff0802f6e2ca02cc6fe748d2519a71700a1eca69
Celine Leano <cemleano@gmail.com>
2019-01-04 13:41:13 -0500

    Fixed printArray function to actually work as intended. Added an include file to index.php.

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
     <h1>Pair Program 1</h1>
 
     <?php
+    include 'functions.php';
+
     $numbers = array(7, 9, 8, 9, 8, 8, 6);
 
-    function printArray($array) {
-        echo $array;
-    }
+    // call printArray method
+    printArray($numbers);
     ?>
 </body>
 </html>
